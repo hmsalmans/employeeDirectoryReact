209e5a613894a2e99b96cc102f516299418a8b3d
hafiz salman <hmsalmans1@gmail.com>
2020-06-30 03:30:28 +0500

    replaced code bootstrap in freindcard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
+import Container from "./components/Container";
 import Title from "./components/Title";
 import friends from "./friends.json";
-import Row from "./components/Row";
-import Col from "./components/Col";
-import Container from "./components/Container";
+
+import "./App.css";
+
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
@@ -13,65 +14,65 @@ class App extends Component {
     friends,
   };
 
-  sortEmployee = (id) => {
+  /*sortEmployee = (id) => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter((friend) => friend.id !== id);
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
-  };
+  };*/
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Employee Directory</Title>
-        <Container> 
-          <Row>
-            <Col size="md-12">
-          <form className="search" style={{alignContent: "auto"}}>
-            <div className="form-group">
-              <input
-                value={this.state.userInput}
-                name="userInput"
-                type="text"
-                className="form-control"
-                placeholder="Search"
-                id="employee"
-                onChange={this.handleInputChange}
-                style={{
-                  width: "100%",
-                  margin: "0 auto",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  textAlign: "center",
-                  
-                }}
-              />{" "}
-            </div>{" "}
-          </form>
-          </Col>
-          </Row>
-          <Row>      
-        {this.state.friends.map((friend) => (
-          
-          <Col size="md-12">
-          <FriendCard
-          sortEmployee={this.sortEmployee}
-            // removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            phone={friend.phone}
-          />
-          </Col>
+      <Container>
 
-         
+        <form className="search" style={{ alignContent: "auto" }}>
+          <div className="form-group">
+            <input
+              value={this.state.userInput}
+              name="userInput"
+              type="text"
+              className="form-control"
+              placeholder="Search"
+              id="employee"
+              onChange={this.handleInputChange}
+              style={{
+                width: "100%",
+                margin: "0 auto",
+                marginLeft: "auto",
+                marginRight: "auto",
+                textAlign: "center",
+              }}
+            />
+          </div>
+        </form>
 
-        ))} 
-        </Row>
-        </Container> 
+        <div>
+          <div className="row">
+            <div className="col">
+              <button className="card-btn">
+                Employee Picture
+              </button>
+            </div>
+            <div className="col">
+              <button className="card-btn">Name</button>
+            </div>
+            <div className="col">
+              <button className="card-btn">Phone</button>
+            </div>
+            <div className="col">
+              <button className="card-btn">Email</button>
+            </div>
+            <div className="col">
+              <button className="card-btn">DOB</button>
+            </div>
+          </div>
+        </div>
+
+        <FriendCard friends={this.state.friends} />
+        </Container>
       </Wrapper>
     );
   }
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
-  return (
-    <div>
-        <img alt={props.name} src={props.image} className={{width:"25%"}} />&nbsp;&nbsp;&nbsp;
-        <strong>Name:</strong> {props.name}  &nbsp; 
-        <strong>Occupation:</strong> {props.occupation} &nbsp;
-        <strong>Phone:</strong> {props.phone}
-        
-      
-      
-      <span onClick={() => props.sortEmployee(props.id)} className="remove">
-        
-      </span>
-    </div>
-  );
-}
 
-export default FriendCard;
+
+
+    function FriendCard(props) { 
+      return (props.friends.map(friend => (        
+      <div className="row" key={friend.id}>       
+       <div className="col img-fluid" >         
+        <img alt={friend.name} src={friend.image} />
+        </div>        
+        <div className="col">{friend.name}</div>        
+        <div className="col">{friend.phone}</div>       
+         <div className="col">{friend.email}</div>       
+          <div className="col">{friend.dob}</div>      
+       </div>      )));}
+    export default FriendCard;
+
+
+
+
+
+
